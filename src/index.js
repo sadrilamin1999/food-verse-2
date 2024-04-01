3c7ae2488fb1f31dd32cb6bdbe0d322ef2c94d33
sadrilamin1999 <sadrilshuvo420@gmail.com>
2024-04-01 21:54:07 +0600

    routes done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./index.css";
 
+import NotFound from "./pages/NotFound";
+import Home from "./pages/Home";
+
+import LayOut from "./LayOut";
+import Favourite from "./pages/Favourite";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LayOut />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/favourite",
+        element: <Favourite />,
+      },
+    ],
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
